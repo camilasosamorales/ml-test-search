48aac1863a138efee26bbc5c8b24a9cb9941b775
csosa <csosa@piconsulting.com.ar>
2019-04-13 16:28:10 -0400

    changing location of SearchBox

--- src/containers/searchResults/SearchResultsContainer.js
+++ src/containers/searchResults/SearchResultsContainer.js
@@ -3,11 +3,14 @@ import React, {Component} from "react";
 
 class SearchResultsContainer extends React.Component{
 
+    componentDidMount(){
+        //cargamos los resultados
+    }
     render(){
         return (
             <div className="search-page container">
             <div className="search-page-results">
-                {(results && results.length) ? results.map(this.renderItem) : null}
+                {/* {(results && results.length) ? results.map(this.renderItem) : null} */}
             </div>
         </div>
         )
